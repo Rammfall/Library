fa81b2825f72e6dc6298064e63aa5526c5bae7d9
Bohdan Onatsky <15bogdan17@gmail.com>
2019-06-20 08:28:07 +0300

    Writing top method

--- modules/statistic.rb
+++ modules/statistic.rb
@@ -10,7 +10,7 @@ class Statistic
     sort_reader = {}
 
     @data['Orders'].each do |reader|
-      if ( readers[reader.reader] )
+      if (readers[reader.reader])
         readers[reader.reader] += 1
       else
         readers[reader.reader] = 1
@@ -23,26 +23,28 @@ class Statistic
 
     sort_reader = sort_reader.to_a
 
-    sort_reader = sort_reader.to_a.sort_by { |value| value }.reverse
+    sort_reader = sort_reader.to_a.sort_by {|value| value}.reverse
 
     print sort_reader[0..quantity]
   end
 
   def top_books(quantity = 1)
-    books = {}
-
-    @data['Orders'].each do |book|
-      if ( books[book.book] )
-        books[book.book] += 1
-      else
-        books[book.book] = 1
-      end
-    end
-
-    print books
+    print top 'Orders', 'book'
   end
 
-  def top
+  def top(entity, column)
+    data = Hash.new
+    sort_data = {}
 
+    @data[entity.to_s].each do |reader|
+      data[reader[column]] = data[reader[column]] ? data[reader[column]] + 1 : 1
+      # print column.to_sym
+      # print data.class
+    end
+
+    data.each do |reader, quantity_books|
+      sort_data[quantity_books] = reader
+    end
+    sort_data
   end
 end
